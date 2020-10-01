4d4a81df0c266702a09e966b5c0ed39a7d6764c5
Aliaksei <AliakseiKo@gmail.com>
2020-10-01 17:11:39 +0300

    rebase config file

--- config/index.js
+++ config/index.js
@@ -1,4 +1,10 @@
+const {
+  port = 1234
+} = require('smargparser')(); // My library. You can replace with minimist.
+
 module.exports = {
-  server: require('./server'),
-  database: require('./database')
+  server: {
+    port
+  },
+  database: {}
 }
--- config/server.js
+++
@@ -1,7 +0,0 @@
-const {
-  port = 1234
-} = require('smargparser')(); // My library. You can replace with minimist.
-
-module.exports = {
-  port
-};
